100c1ec3905b4a3680ae696b80d7234b5a8012fd
Toni <anton.krizmanic94@gmail.com>
2025-08-29 08:17:13 +0200

    remove padding

--- src/components/social/whats-app-card.tsx
+++ src/components/social/whats-app-card.tsx
@@ -7,7 +7,7 @@ export function WhatsAppCard() {
             href="https://chat.whatsapp.com/"
             ctaText="Pridružite nam se na WhatsApp-u"
             icon={<WhatsAppIcon className="size-10 fill-white" />}
-            bgColor="bg-gradient-to-br p-2 from-green-50 to-emerald-50 border-green-200"
+            bgColor="bg-gradient-to-br from-green-50 to-emerald-50 border-green-200"
             bgIconColor="bg-green-500"
             navigateIconColor="text-green-600"
         />
