e4fcf8e0c54dce41aa3c21924e5f35a090fd9870
Anton Krizmanic <anton.krizmanic94@gmail.com>
2025-12-29 18:57:40 +0100

    Calendar 2026

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,11 @@ import {
 import { ProcessSection } from '@/components/sections/ProcessSection';
 
 export default function Home() {
-    const actions = [
+    const actions = [        
+        // 2026
         {
-            date: new Date('2025-09-16'),
-            title: '16.09.2025. – Grad Buje',
-            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
-            location: 'Zajednica Talijana Buje',
-            address: 'Trg Josipa Broza Tita 17',
-            description:
-                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
-        },
-        {
-            date: new Date('2025-10-17'),
-            title: '17.10.2025. – Grad Novigrad',
+            date: new Date('2026-01-16'),
+            title: '16.01.2026. – Grad Novigrad',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Sportska dvorana',
             address: null,
@@ -31,8 +23,8 @@ export default function Home() {
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
         {
-            date: new Date('2025-11-14'),
-            title: '14.11.2025. – Grad Buje',
+            date: new Date('2026-02-13'),
+            title: '13.02.2026. – Grad Buje',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Buje',
             address: 'Trg Josipa Broza Tita 17',
@@ -40,8 +32,99 @@ export default function Home() {
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
         {
-            date: new Date('2025-12-29'),
-            title: '29.12.2025. – Grad Umag',
+            date: new Date('2026-03-24'),
+            title: '24.03.2026. – Grad Umag',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Umag',
+            address: 'Brolo 1, Umag',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-04-10'),
+            title: '10.04.2026. – Grad Novigrad',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Sportska dvorana',
+            address: null,
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },        
+        {
+            date: new Date('2026-04-27'),
+            title: '27.04.2026. – Grad Umag',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Umag',
+            address: 'Brolo 1, Umag',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-05-13'),
+            title: '13.05.2026. – Grad Buje',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Buje',
+            address: 'Trg Josipa Broza Tita 17',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-06-23'),
+            title: '23.06.2026. – Grad Umag',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Umag',
+            address: 'Brolo 1, Umag',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-07-13'),
+            title: '13.07.2026. – Grad Novigrad',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Sportska dvorana',
+            address: null,
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },       
+        
+        {
+            date: new Date('2026-08-12'),
+            title: '12.08.2026. – Grad Buje',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Buje',
+            address: 'Trg Josipa Broza Tita 17',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-09-21'),
+            title: '21.09.2026. – Grad Umag',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Umag',
+            address: 'Brolo 1, Umag',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-10-12'),
+            title: '12.10.2026. – Grad Novigrad',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Sportska dvorana',
+            address: null,
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },        
+        {
+            date: new Date('2026-11-09'),
+            title: '09.11.2026. – Grad Buje',
+            time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
+            location: 'Zajednica Talijana Buje',
+            address: 'Trg Josipa Broza Tita 17',
+            description:
+                'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
+        },
+        {
+            date: new Date('2026-12-22'),
+            title: '22.12.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
             address: 'Brolo 1, Umag',
