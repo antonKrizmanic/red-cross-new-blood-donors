7496dbd163bdca01b5529bf677c18b1c5baa50f8
Toni <anton.krizmanic94@gmail.com>
2026-01-15 18:26:55 +0100

    zip and city

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ import {
 import { ProcessSection } from '@/components/sections/ProcessSection';
 
 export default function Home() {
-    const actions = [        
+    const actions = [
         // 2026
         {
             date: new Date('2026-01-16'),
             title: '16.01.2026. – Grad Novigrad',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Sportska dvorana',
-            address: "Marketi 2",
+            address: "Marketi 2, 52466 Novigrad",
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -27,7 +27,7 @@ export default function Home() {
             title: '13.02.2026. – Grad Buje',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Buje',
-            address: 'Trg Josipa Broza Tita 17',
+            address: 'Trg Josipa Broza Tita 17, 52460 Buje',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -36,7 +36,7 @@ export default function Home() {
             title: '24.03.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
-            address: 'Brolo 1, Umag',
+            address: 'Brolo 1, 52470 Umag',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -45,16 +45,16 @@ export default function Home() {
             title: '10.04.2026. – Grad Novigrad',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Sportska dvorana',
-            address: "Marketi 2",
+            address: "Marketi 2, 52466 Novigrad",
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
-        },        
+        },
         {
             date: new Date('2026-04-27'),
             title: '27.04.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
-            address: 'Brolo 1, Umag',
+            address: 'Brolo 1, 52470 Umag',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -63,7 +63,7 @@ export default function Home() {
             title: '13.05.2026. – Grad Buje',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Buje',
-            address: 'Trg Josipa Broza Tita 17',
+            address: 'Trg Josipa Broza Tita 17, 52460 Buje',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -72,7 +72,7 @@ export default function Home() {
             title: '23.06.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
-            address: 'Brolo 1, Umag',
+            address: 'Brolo 1, 52470 Umag',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -81,17 +81,17 @@ export default function Home() {
             title: '13.07.2026. – Grad Novigrad',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Sportska dvorana',
-            address: "Marketi 2",
+            address: "Marketi 2, 52466 Novigrad",
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
-        },       
-        
+        },
+
         {
             date: new Date('2026-08-12'),
             title: '12.08.2026. – Grad Buje',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Buje',
-            address: 'Trg Josipa Broza Tita 17',
+            address: 'Trg Josipa Broza Tita 17, 52460 Buje',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -100,7 +100,7 @@ export default function Home() {
             title: '21.09.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
-            address: 'Brolo 1, Umag',
+            address: 'Brolo 1, 52470 Umag',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -109,16 +109,16 @@ export default function Home() {
             title: '12.10.2026. – Grad Novigrad',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Sportska dvorana',
-            address: "Marketi 2",
+            address: "Marketi 2, 52466 Novigrad",
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
-        },        
+        },
         {
             date: new Date('2026-11-09'),
             title: '09.11.2026. – Grad Buje',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Buje',
-            address: 'Trg Josipa Broza Tita 17',
+            address: 'Trg Josipa Broza Tita 17, 52460 Buje',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
@@ -127,7 +127,7 @@ export default function Home() {
             title: '22.12.2026. – Grad Umag',
             time: '09:00 - 17:00 (pauza 13:00 - 14:00)',
             location: 'Zajednica Talijana Umag',
-            address: 'Brolo 1, Umag',
+            address: 'Brolo 1, 52470 Umag',
             description:
                 'Za točnu lokaciju prati naše objave na društvenim mrežama i lokalnim medijima.',
         },
